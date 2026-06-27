1dcb0c62c84d442ab7d94cd59aa170fa478f7915
Akimenko_Ivan <412871@niuitmo.ru>
2026-06-27 20:27:39 +0300

    update sandbox main

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+#include <engine/platform/win32/window.hpp>
+
 int main() {
 	std::cout << "CPP-ENGINE-LAB started\n";
+
+	engine::WindowConfig window_cfg{ .title = L"CPP-ENGINE-LAB", .width = 1280, .height = 720 };
+	engine::Window window(window_cfg);
+
+	while (!window.should_close()) {
+		window.poll_events();
+	}
 
 	return 0;
 }
